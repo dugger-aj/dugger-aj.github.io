11ddcf8c335b0f92d4c78bfa78f0a81c5eea3e4a
dugger-aj <aj.dugger11@gmail.com>
2017-10-06 09:02:45 -0600

    asdf

--- js/golf.js
+++ js/golf.js
@@ -11,7 +11,7 @@ xhr.onload = function () {
     if (xhr.status === 200) {
         golfInfo = JSON.parse(xhr.responseText);
         console.log(golfInfo);
-        
+        enterData(0);
     } // end if
 } // end function
 
@@ -20,4 +20,11 @@ function enterData(x){
 	document.getElementById("courseName").innerHTML = golfInfo[x].name;
 	document.getElementById("description").innerHTML = golfInfo[x].description;
 	document.getElementById("photo").src = golfInfo[x].photo;
+	
+	document.getElementById("weekday").innerHTML = golfInfo[x].cost.weekday;
+	document.getElementById("weekend").innerHTML = golfInfo[x].cost.weekend;
+	
+	for(i=0; i < golfInfo[x].details.length; i++){
+		document.getElementById("details").innerHTML = "<p>" + golfInfo[x].details[i] + "</p>";
+	}
 }
