5ca065c8155f1a060f082be09e929780f408d20d
dugger-aj <aj.dugger11@gmail.com>
2017-10-12 15:58:59 -0600

    10/12

--- js/eventsFun.js
+++ js/eventsFun.js
@@ -18,6 +18,7 @@ function augusta(){
 		
 		//creating paragraph and text nodes and appending to div
 		var paragraph = document.createElement("P");
+		paragraph.id = "child";
 		var textForPara = document.createTextNode(smartRemarks[sessionStorage.clickcount-1]);
 
 		paragraph.appendChild(textForPara);
@@ -30,3 +31,51 @@ document.getElementById("clearStorageButton").onclick = function (){
 	sessionStorage.clear();
 	document.getElementById("coolNewStuff").innerHTML = "";
 };
+
+//adding event listener to div
+document.getElementById("removeChild").addEventListener("click", removeChild);
+
+function removeChild(){
+	var parent = document.getElementById("coolNewStuff");
+	
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
+	}
+}
+
+function changeBorder(){
+	document.getElementById("augustaPic").style.borderRadius = "8px";
+}
+
+//creating object
+var valleyView = {
+	name: "Valley View",
+	holes: "18",
+	city: "Layton",
+	state: "UT",
+	price: 44,
+	img: "images/golf.jpg",
+	printThis : function() {
+       return "<p>" + this.name + "</p><p>Number of Holes: " + this.holes + "</p><p>Location: " + this.city + ", " + this.state + "</p><p>Price: " + this.price + "</p>";
+    }
+};
+
+// throwing that object in local storage and retrieving it. I then display it on the page afterwards
+if (typeof(Storage) !== "undefined") {
+	
+    localStorage.setItem('valleyView',JSON.stringify(valleyView));
+	var localValleyView = localStorage.getItem("valleyView");
+	var parsedValleyViewObject = JSON.parse(localValleyView);
+	var valleyViewPicture = document.createElement("IMG");
+	valleyViewPicture.setAttribute("src", parsedValleyViewObject.img);
+	document.getElementById("valleyViewWrapper").appendChild(valleyViewPicture);
+	
+} else {
+    alert("No support for web storage");
+}
+
+function doThis (){
+	
+	document.getElementById("valleyViewInfo").innerHTML = valleyView.printThis();	
+	
+}
